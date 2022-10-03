95df9a0c9c8d6d3d0b233d39021078406c6cb34b
Niken Riri <nikenriri05@gmail.com>
2022-10-03 14:01:40 +0700

    Upload Final Project HTML&CSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+module.exports={
+    content: ['index.html'],
+    theme:{
+        container:{
+            center: true,
+            padding: '16px',
+        },
+        extend:{
+            colors:{
+                primary: '#14b8a6',
+            },
+            screen:{
+                '2xl': '1320px',
+            },
+        },
+    },
+    plugin:[],
+} 
